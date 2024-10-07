c91795ebacfaf439fe6af1903f4de5870af29d6d
AbdullahBoreny <abdullah55boreny@gmail.com>
2024-10-07 06:35:43 +0300

    setting up the calculter and adding styles to it, the remaining parts is to make it intractive with script.js

---
+++ script.js
@@ -0,0 +1,41 @@
+function main(){
+    let number1;
+    let operator;
+    let number2;
+    
+    
+    function operate(number1,operator,number2){
+    
+    }
+    };
+    
+
+
+
+const add = function(a,b) {
+	return a +b;
+};
+
+const subtract = function(a,b) {
+	return a - b;
+};
+
+const sum = function(array) {
+	let sum = 0;
+  array.forEach(element => {
+    sum = sum +element;
+  });
+  return sum;
+};
+
+const multiply = function(array) {
+  let number =1;
+array.forEach(element => {
+  number = number * element;
+});
+return number;
+};
+
+const power = function(a,b) {
+	return a**b;
+};
